613cfabab6b7f3c21300ce68c2c248def18f2c9c
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2023-10-25 14:41:04 +0530

    updated user-service 251023
    user-service is registered with eureka

--- src/main/java/com/boot/hotel/UserServiceApplication.java
+++ src/main/java/com/boot/hotel/UserServiceApplication.java
@@ -2,9 +2,10 @@ package com.boot.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 //@EnableFeignClients
 public class UserServiceApplication {
 	
